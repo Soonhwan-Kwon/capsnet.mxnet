be4f6d42c48a17647355748a4050615fcabcc0b9
Soonhwan-Kwon <soouee@gmail.com>
2017-11-13 19:26:58 +0900

    fix last layer relu to sigmoid fix issue related to #3 fix issue related to #2 set mnist force_download option to False

--- layer_capsnet.py
+++ layer_capsnet.py
@@ -12,37 +12,37 @@ input_shape =(1, 28, 28)
 # net.shape = [batch_size, 256, 20, 20]
 #net = mx.sym.Reshape(data=data, shape=(0., -4, -1, 1, 0, 0))
 #print('after reshape', net.infer_shape(data=(batch_size, 28, 28)))
-net = mx.sym.Convolution(data=data,
+conv1 = mx.sym.Convolution(data=data,
                          num_filter=conv1_num_filter,
                          kernel=kernel,
                          layout='NCHW',
                          name='conv1')
-net = mx.sym.Activation(data=net, act_type='relu', name='conv1_act')
-print('after conv2d',net.infer_shape(data=(batch_size, 1, 28, 28)))
+conv1 = mx.sym.Activation(data=conv1, act_type='relu', name='conv1_act')
+print('after conv2d',conv1.infer_shape(data=(batch_size, 1, 28, 28)))
 # net.shape = [batch_size, 256, 6, 6]
 dim_vector = 8
 n_channels = 32
 squash_axis = 2
-net = mx.sym.Convolution(data=net,
+primarycaps = mx.sym.Convolution(data=conv1,
                          num_filter=dim_vector*n_channels,
                          kernel=kernel,
                          stride=[2, 2],
                          name='conv2_primarycap'
                          )
-print('after conv2d 2',net.infer_shape(data=(batch_size, 1, 28, 28)))
+print('after conv2d 2',primarycaps.infer_shape(data=(batch_size, 1, 28, 28)))
 # net.shape = [batch_size, 1152,8]
-net = mx.sym.Reshape(data=net, shape=(0, -1, dim_vector))
+primarycaps = mx.sym.Reshape(data=primarycaps, shape=(0, -1, dim_vector))
 
 
-def squash(net, name=''):
+def squash(net, squash_axis, name=''):
     s_squared_norm = mx.sym.sum(data=mx.sym.square(net, name='square_'+name), axis=squash_axis, keepdims=True, name='s_squared_norm_'+name)
     scale = s_squared_norm / (1 + s_squared_norm) / mx.sym.sqrt(data=(s_squared_norm+epsilon), name='s_squared_norm_sqrt_'+name)
     squashed_net = mx.sym.broadcast_mul(scale, net, name='squashed_net_'+name)
     return squashed_net
 
-net = squash(net)
-net.infer_shape(data=(batch_size, 1, 28, 28))
-print('after squash', net.infer_shape(data=(batch_size, 1, 28, 28)))
+primarycaps = squash(primarycaps, squash_axis)
+primarycaps.infer_shape(data=(batch_size, 1, 28, 28))
+print('after squash', primarycaps.infer_shape(data=(batch_size, 1, 28, 28)))
 # CapsuleLayer
 
 # int_num_capsule = n_class
@@ -82,18 +82,18 @@ bias = mx.sym.Variable('Bias',
 
 # ('input', TensorShape([Dimension(None), Dimension(1152), Dimension(8)]))
 # ('inputs_expand', TensorShape([Dimension(None), Dimension(1152), Dimension(1), Dimension(8), Dimension(1)]))
-net = mx.sym.Reshape(data=net, shape=(0, 0, -4, -1, 1))
-net = mx.sym.Reshape(data=net, shape=(0, 0, -4, 1, -1, 0))
-print('after inputs_expand', net.infer_shape(data=(batch_size, 1, 28, 28)))
+inputs_expand = mx.sym.Reshape(data=primarycaps, shape=(0, 0, -4, -1, 1))
+inputs_expand = mx.sym.Reshape(data=inputs_expand, shape=(0, 0, -4, 1, -1, 0))
+print('after inputs_expand', inputs_expand.infer_shape(data=(batch_size, 1, 28, 28)))
 # input_tiled
 # ('inputs_tiled.shape', TensorShape([Dimension(None), Dimension(1152), Dimension(10), Dimension(8), Dimension(1)]))
-net = mx.sym.tile(data=net, reps=(1, 1, n_class, 1, 1))
-net.infer_shape(data=(batch_size, 1, 28, 28))
+inputs_tiled = mx.sym.tile(data=inputs_expand, reps=(1, 1, n_class, 1, 1))
+inputs_tiled.infer_shape(data=(batch_size, 1, 28, 28))
 # w_tiled
 # w_tiled.shape = [(1L, 1152L, 10L, 8L, 16L)]
 w_tiled = mx.sym.tile(w, reps=(batch_size, 1, 1, 1, 1))
 w_tiled.infer_shape()
-inputs_hat = mx.sym.linalg_gemm2(w_tiled, net, transpose_a=True)
+inputs_hat = mx.sym.linalg_gemm2(w_tiled, inputs_tiled, transpose_a=True)
 inputs_hat = mx.sym.swapaxes(data=inputs_hat, dim1=3, dim2=4)
 inputs_hat.infer_shape(data=(batch_size, 1, 28, 28))
 # ('i', 0, 'c', TensorShape([Dimension(1), Dimension(1152), Dimension(10), Dimension(1), Dimension(1)]))
@@ -101,10 +101,10 @@ inputs_hat.infer_shape(data=(batch_size, 1, 28, 28))
 for i in range(0, num_routing):
     c = mx.sym.softmax(bias, axis=2, name='c'+str(i))
     #print('i',i,' c',c.infer_shape())
-    outputs = squash(mx.sym.sum(mx.sym.broadcast_mul(c, inputs_hat, name='broadcast_mul_'+str(i)), axis=1, keepdims=True, name='sum_'+str(i)), name='output_'+str(i))
+    outputs = squash(mx.sym.sum(mx.sym.broadcast_mul(c, inputs_hat, name='broadcast_mul_'+str(i)), axis=1, keepdims=True, name='sum_'+str(i)), name='output_'+str(i), squash_axis=4)
     #print('i', i, ' output', outputs.infer_shape(data=(1, 28, 28)))
     if i != num_routing - 1:
-        bias += mx.sym.sum(mx.sym.broadcast_mul(c, inputs_hat, name='bias_broadcast_mul'+str(i)), axis=4, keepdims=True, name='bias_'+str(i))
+        bias = bias + mx.sym.sum(mx.sym.broadcast_mul(c, inputs_hat, name='bias_broadcast_mul'+str(i)), axis=4, keepdims=True, name='bias_'+str(i))
 
 # ('digitcaps', TensorShape([Dimension(None), Dimension(10), Dimension(16)]))
 digitcaps = mx.sym.Reshape(data=outputs, shape=(-1, num_capsule, dim_vector))
@@ -122,27 +122,27 @@ digitcaps.infer_shape(data=(batch_size, 1, 28, 28))
 
 out_caps = mx.sym.sqrt(data=mx.sym.sum(digitcaps, 2))
 out_caps.infer_shape(data=(batch_size, 1, 28, 28))
-y = mx.sym.Variable('softmax_label', shape=(batch_size))
+y = mx.sym.Variable('softmax_label', shape=(batch_size,))
 y = mx.sym.one_hot(y, n_class)
 y = mx.sym.Reshape(data=y, shape=(batch_size, -4, n_class, -1))
-y.infer_shape(softmax_label=(batch_size))
+y.infer_shape(softmax_label=(batch_size,))
 #inputs_masked = mx.sym.batch_dot(mx.sym.transpose(y), digitcaps)
 inputs_masked = mx.sym.linalg_gemm2(y, digitcaps, transpose_a=True)
 #inputs_masked = mx.sym.linalg_gemm(mx.sym.transpose(y), digitcaps, transpose_a=True)
 # y.infer_shape(y=(1,10))
 # inputs_masked = mx.sym.linalg_gemm(out_caps, y, transpose_a=True)
 #inputs_masked = mx.sym.batch_dot(out_caps, y)
-print(inputs_masked.infer_shape(data=(batch_size, 1, 28, 28), softmax_label=(batch_size)))
+print(inputs_masked.infer_shape(data=(batch_size, 1, 28, 28), softmax_label=(batch_size,)))
 inputs_masked = mx.sym.Reshape(data=inputs_masked, shape=(-3, 0))
-print(inputs_masked.infer_shape(data=(batch_size, 1, 28, 28), softmax_label=(batch_size)))
+print(inputs_masked.infer_shape(data=(batch_size, 1, 28, 28), softmax_label=(batch_size,)))
 x_recon = mx.sym.FullyConnected(data=inputs_masked, num_hidden=512, name='x_recon')
 x_recon = mx.sym.Activation(data=x_recon, act_type='relu', name='x_recon_act')
 x_recon = mx.sym.FullyConnected(data=x_recon, num_hidden=1024, name='x_recon2')
 x_recon = mx.sym.Activation(data=x_recon, act_type='relu', name='x_recon_act2')
 
 x_recon = mx.sym.FullyConnected(data=x_recon, num_hidden=np.prod(input_shape), name='x_recon3')
-x_recon = mx.sym.Activation(data=x_recon, act_type='relu', name='x_recon_act3')
-print(x_recon.infer_shape(data=(batch_size, 1, 28, 28), softmax_label=(batch_size)))
+x_recon = mx.sym.Activation(data=x_recon, act_type='sigmoid', name='x_recon_act3')
+print(x_recon.infer_shape(data=(batch_size, 1, 28, 28), softmax_label=(batch_size,)))
 def margin_loss(y_true, y_pred):
     L = y_true * mx.sym.square(mx.sym.maximum(0., 0.9 - y_pred)) + \
         0.5 * (1 - y_true) * mx.sym.square(mx.sym.maximum(0., y_pred - 0.1))
@@ -160,7 +160,7 @@ import os
 import urllib
 import gzip
 import struct
-def download_data(url, force_download=True):
+def download_data(url, force_download=False):
     fname = url.split("/")[-1]
     if force_download or not os.path.exists(fname):
         urllib.urlretrieve(url, fname)
@@ -189,20 +189,17 @@ val_iter = mx.io.NDArrayIter(to4d(val_img), val_lbl, batch_size)
 
 class LossMetric(mx.metric.EvalMetric):
     def __init__(self, batch_size, num_gpu, is_epoch_end=False, is_logging=True):
-        super(LossMetric, self).__init__('STTMetric')
+        super(LossMetric, self).__init__('LossMetric')
         self.batch_size = batch_size
         self.num_gpu = num_gpu
     def update(self, labels, preds):
         self.batch_loss = 0.
-        for label, pred in zip(labels[0],preds[0]):
+        for label, pred_softmax, pred_recon in zip(labels[0], preds[0], preds[1]):
             print(label.asnumpy())
-            print(pred.asnumpy())
-
+            print(pred_softmax.asnumpy())
+            print(pred_recon.asnumpy())
     def get_batch_loss(self):
         return self.batch_loss
-    def get_name_value(self):
-        total_cer = float(self.total_l_dist) / float(self.total_n_label)
-        return total_cer, self.total_n_label, self.total_l_dist, self.total_ctc_loss
     def reset(self):
         self.total_n_label = 0
         self.total_l_dist = 0
